f3ee87e9835a64b09bee7303263f0a9689232a6c
Santosh <santoshmajhi99@gmail.com>
2017-07-18 11:20:22 +0530

    first commit

---
+++ controller/main_controller.js
@@ -0,0 +1,6 @@
+app.controller("Main_Controller",function($scope,$rootScope,$state,$localStorage){
+  /*******************************************************/
+  /*************This is use for check user login**********/
+  /*******************************************************/
+  
+});
---
+++ main_app.js
@@ -0,0 +1,92 @@
+var app = angular.module("acc_app", ['ui.router', 'ui.bootstrap', 'ngResource', 'ngStorage', 'ngAnimate','datePicker','ngTable']);
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/dashboard');
+  $stateProvider
+  .state('login', {
+      templateUrl: 'views/login.html',
+      url: '/login',
+      //resolve: {
+        //loggedout: checkLoggedin
+      //}
+  })
+  .state('dashboard', {
+    templateUrl: 'views/dashboard.html',
+    url: '/dashboard',
+    //resolve: {
+      //loggedout: checkLoggedout
+    //}
+  })
+
+  function checkLoggedout($q, $timeout, $rootScope, $state, $localStorage) {
+    var deferred = $q.defer();
+    accessToken = localStorage.getItem('accessToken')
+    $timeout(function(){
+      if(accessToken){
+        deferred.resolve();
+      }
+      else{
+        deferred.resolve();
+        $state.go('login');
+      }
+    },100)
+  }
+  function checkLoggedin($q, $timeout, $rootScope, $state, $localStorage) {
+    var deferred = $q.defer();
+    accessToken = localStorage.getItem('accessToken')
+    $timeout(function(){
+      if(accessToken){
+        deferred.resolve();
+        $state.go('dashboard');
+      }
+      else{
+        deferred.resolve();
+      }
+    },100)
+  }
+});
+app.constant('CONFIG', {
+  'HTTP_HOST': 'server/api.php'
+})
+app.run(function($http,$rootScope,$localStorage,$timeout){
+  $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    $rootScope.stateName = toState.name;
+  })
+});
+app.factory('Util', ['$rootScope',  '$timeout' , function( $rootScope, $timeout){
+    var Util = {};
+    $rootScope.alerts =[];
+    Util.alertMessage = function(msgType, message){
+        var alert = { type:msgType , msg: message };
+        $rootScope.alerts.push( alert );
+        $timeout(function(){
+            $rootScope.alerts.splice($rootScope.alerts.indexOf(alert), 1);
+        }, 5000);
+    };
+    return Util;
+}]);
+
+app.directive('fileModel', ['$parse', function ($parse) {
+    return {
+       restrict: 'A',
+       link: function(scope, element, attrs) {
+          var model = $parse(attrs.fileModel);
+          var modelSetter = model.assign;
+
+          element.bind('change', function(){
+             scope.$apply(function(){
+                modelSetter(scope, element[0].files[0]);
+             });
+          });
+       }
+    };
+ }]);
+ app.filter('getShortName', function () {
+     return function (value) {
+       if(value){
+         var temp = angular.copy(value);
+         temp = temp.split(" ");
+         temp = temp[0].charAt(0)+temp[temp.length-1].charAt(0);
+         return temp.toUpperCase();
+       }
+     };
+ });
